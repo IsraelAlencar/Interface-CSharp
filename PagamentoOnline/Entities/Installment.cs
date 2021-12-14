43a67c6045a8e837f331ce6669d86e501baa58a2
IsraelAlencar <israelalencarribeiro@gmail.com>
2021-12-14 17:10:48 -0400

    exercicio de fixacao interface (pagamento online)

---
+++ PagamentoOnline/Entities/Installment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PagamentoOnline.Entities {
+    class Installment {
+        public DateTime DueDate { get; set; }
+        public double Amount { get; set; }
+
+        public Installment(DateTime date, double amount) {
+            DueDate = date;
+            Amount = amount;
+        }
+
+        public override string ToString() {
+            return DueDate
+                + " - "
+                + Amount;
+        }
+    }
+}
---
+++ PagamentoOnline/Services/PaypalService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PagamentoOnline.Services {
+    class PaypalService : IOnlinePaymentService {
+        private const double MonthlyInterest = 0.01;
+        private const double FeePercentage = 0.02;
+
+        public double Interest(double amount, int months) {
+            return amount * MonthlyInterest * months;
+        }
+
+        public double PaymentFee(double amount) {
+            return amount * FeePercentage;
+        }
+    }
+}
